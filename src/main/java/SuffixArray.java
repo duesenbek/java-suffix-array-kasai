2f56968eaa7b23c38df01cca0880cf64f97c897f
duesenbek <duesenbek@icloud.com>
2025-11-13 20:29:39 +0500

    feat:basic Suffix Array realization  from lecture 10

--- src/main/java/SuffixArray.java
+++ src/main/java/SuffixArray.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class SuffixArray {
-}
+    public static int[] buildSuffixArray(String s) {
+        int n = s.length();
+
+        Integer[] helper = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            helper[i] = i;
+        }
+        Arrays.sort(helper, (a, b) -> s.substring(a).compareTo(s.substring(b)));
+        int[] suffixArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            suffixArray[i] = helper[i];
+
+        }
+        return suffixArray;
+    }
+        public static  void main(String[] args){
+            String s="banana$";
+
+            int[] SuffixArray = buildSuffixArray(s);
+            System.out.println(s+Arrays.toString(SuffixArray));
+            }
+
+
+
+        }
+
+
